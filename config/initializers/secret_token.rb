4eac6e555f1a5b2568805d3f740ff03cae6e0be4
Jon Botelho <jon@jbotelho.com>
2014-11-10 21:25:52 -0500

    Add default secret token for dev

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,8 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Noshare::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
+Noshare::Application.config.secret_key_base = if Rails.env.production?
+                                                ENV['SECRET_KEY_BASE']
+                                              else
+                                                'f0fdd0221762867d3be9afe1c3889356eebb3a430c0508b038b567f46a4604b769c99e57804b75254a79738474669b07ee4b8ffd691c5feecbf50ac313af2cc0'
+                                              end
